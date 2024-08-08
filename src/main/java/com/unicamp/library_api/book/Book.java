f7a27393707677e16296efa92923e6dd686d3bdc
Beatriz Coutinho <beatrizjcoutinho@gmail.com>
2024-08-08 10:32:38 -0300

    feat: start to implement book entity

--- src/main/java/com/unicamp/library_api/book/Book.java
+++ src/main/java/com/unicamp/library_api/book/Book.java
@@ -1,9 +1,12 @@
 package com.unicamp.library_api.book;
 
 import com.unicamp.library_api.author.Author;
+import com.unicamp.library_api.publisher.Publisher;
+
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -25,7 +28,17 @@ public class Book {
     @Column(nullable = false)
     private String isbn;
 
+    @Column(nullable = false)
+    private String location;
+
+    @Column(name = "publication_date", nullable = false)
+    private LocalDateTime publicationDate;
+
     @ManyToOne
     @JoinColumn(name = "author_id", nullable = false)
     private Author author;
+
+    @ManyToOne
+    @JoinColumn(name = "publisher_id", nullable = false)
+    private Publisher publisher;
 }
---
+++ src/main/java/com/unicamp/library_api/publisher/Publisher.java
@@ -0,0 +1,44 @@
+package com.unicamp.library_api.publisher;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.unicamp.library_api.book.Book;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "publisher", cascade = CascadeType.ALL)
+    private List<Book> books;
+}
